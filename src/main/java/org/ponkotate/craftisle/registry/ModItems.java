b73a872cea589b74cf6b3d8c601e8d305bbfea82
ponkotate <ponkotate@gmail.com>
2026-05-10 18:59:47 +0900

    feat(items): register pebble and plastic_rope to Ingredients tab, stone_knife to Tools tab

--- src/main/java/org/ponkotate/craftisle/registry/ModItems.java
+++ src/main/java/org/ponkotate/craftisle/registry/ModItems.java
@@ -1,11 +1,13 @@
 package org.ponkotate.craftisle.registry;
 
+import net.fabricmc.fabric.api.creativetab.v1.CreativeModeTabEvents;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.Identifier;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 
@@ -29,5 +31,17 @@ public class ModItems {
         return Registry.register(BuiltInRegistries.ITEM, key, new BlockItem(block, new Item.Properties().setId(key)));
     }
 
-    public static void initialize() {}
+    private static ResourceKey<CreativeModeTab> tabKey(String name) {
+        return ResourceKey.create(Registries.CREATIVE_MODE_TAB, Identifier.withDefaultNamespace(name));
+    }
+
+    public static void initialize() {
+        CreativeModeTabEvents.modifyOutputEvent(tabKey("ingredients")).register(output -> {
+            output.accept(PEBBLE);
+            output.accept(PLASTIC_ROPE);
+        });
+        CreativeModeTabEvents.modifyOutputEvent(tabKey("tools_and_utilities")).register(output -> {
+            output.accept(STONE_KNIFE);
+        });
+    }
 }
